23603234e156086de39a8fbf5a2ae8f55dcff6de
Stefan Schwaighofer <stefan.schwaighofer@hs-augsburg.de>
2022-12-08 09:17:16 +0100

    start container script

--- start-db.sh
+++ start-db.sh
@@ -1,1 +1,16 @@
-docker run -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=password -p 5984:5984 --name hsa-couchsurfer -d couchdb:latest
+if [[ -z "${COUCHDB_USER}" || -z "${COUCHDB_PASSWORD}" ]]; then
+    echo "Missing environment variable COUCHDB_USER and/or COUCHDB_PASSWORD"
+    echo ""
+    echo "Set them either before running this script (1) or export them for the current shell session (2):"
+    echo "1) COUCHDB_USER=... COUCHDB_PASSWORD=... ./start-db.sh"
+    echo ""
+    echo "2) export COUCHDB_USER=..."
+    echo "export COUCHDB_PASSWORD=..."
+    echo "./start-db.sh"
+else
+    res=$(docker start hsa-couchsurfer 2> /dev/null)
+
+    if [[ -z $res ]]; then
+        docker run -e COUCHDB_USER=${COUCHDB_USER} -e COUCHDB_PASSWORD=${COUCHDB_PASSWORD} -p 5984:5984 --name hsa-couchsurfer -d couchdb:latest
+    fi
+fi
